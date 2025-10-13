e16cebe75bb44b447cc378dbd40c96d21b5508ee
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-10-13 20:54:06 +0900

    Settings: Handle NPE in HideDeveloperStatusSettings
    Change-Id: I1e9ed05151c1f2931aab76cc64ad80dfe2f8376c

--- src/com/android/settings/security/HideDeveloperStatusSettings.kt
+++ src/com/android/settings/security/HideDeveloperStatusSettings.kt
@@ -83,9 +83,9 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
         val cr = requireContext().contentResolver
 
         val apps =
-            Settings.Secure.getString(cr, Settings.Secure.HIDE_DEVELOPER_STATUS)
-                .split(",")
-                .toMutableSet()
+            Settings.Secure.getString(cr, getKey())
+                ?.split(",")
+                ?.toMutableSet() ?: mutableSetOf<String>()
 
         when (action) {
             Action.ADD -> apps.add(packageName)
@@ -95,7 +95,7 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
 
         Settings.Secure.putStringForUser(
             cr,
-            Settings.Secure.HIDE_DEVELOPER_STATUS,
+            getKey(),
             apps.joinToString(separator = ","),
             userId,
         )
